365b0a07dda459d8b8e65963e688cbe35096cefa
Cameron Clough <cameronjclough@gmail.com>
2019-09-07 00:26:20 +0100

    docs: ✏️ document ByteUtils

--- src/main/java/com/gitlab/incognitojam/ext2/ByteUtils.java
+++ src/main/java/com/gitlab/incognitojam/ext2/ByteUtils.java
@@ -1,6 +1,23 @@
 package com.gitlab.incognitojam.ext2;
 
+/**
+ * Utility methods relating to bytes.
+ */
 public class ByteUtils {
+    /**
+     * Format an number of bytes with the matching suffix.
+     *
+     * For example, see how 1_000_000L is formatted:
+     * ```
+     * long bytes = 1_000_000L;
+     * String bytesLabel = ByteUtils.formatHumanReadable(bytes);
+     *
+     * bytesLabel.equals("976.6 KiB"); // true
+     * ```
+     *
+     * @param bytes the value of bytes to format
+     * @return returns formatted string with correct suffix
+     */
     public static String formatHumanReadable(long bytes) {
         final int unit = 1024;
 
