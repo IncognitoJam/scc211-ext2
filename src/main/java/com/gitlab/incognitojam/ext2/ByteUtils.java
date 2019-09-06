037079a8c8b1b60d80ac21514de57b0c92b78934
Cameron Clough <cameronjclough@gmail.com>
2019-09-07 00:36:19 +0100

    docs: ✏️ add line comments to ByteUtils

--- src/main/java/com/gitlab/incognitojam/ext2/ByteUtils.java
+++ src/main/java/com/gitlab/incognitojam/ext2/ByteUtils.java
@@ -21,9 +21,27 @@ public class ByteUtils {
     public static String formatHumanReadable(long bytes) {
         final int unit = 1024;
 
+        /*
+         * If the number of bytes is less than the lowest unit (1 MiB), just
+         * append " B" and return.
+         */
         if (bytes < unit) return bytes + " B";
-        int exp = (int) (Math.log(bytes) / Math.log(unit));
-        String pre = "KMGTPE".charAt(exp - 1) + "i";
-        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+
+        /*
+         * Determine the "exponent" of the unit, or how many times you would
+         * need to multiply the unit by itself to get to this number of bytes.
+         *
+         * This tells us which suffix to use.
+         */
+        int exponent = (int) (Math.log(bytes) / Math.log(unit));
+
+        // Get the correct suffix.
+        String suffix = "KMGTPE".charAt(exponent - 1) + "iB";
+
+        // Convert number of bytes to new unit (if MB then divide by 1 mil...).
+        double value = bytes / Math.pow(unit, exponent);
+
+        // Round the value to 1 DP and format it!
+        return String.format("%.1f %s", value, suffix);
     }
 }
