41c2757f0e314a3f987f446b539b7eaa7440d69a
Cameron Clough <cameronjclough@gmail.com>
2019-11-30 22:07:01 +0000

    test(ext2-file): test root file attributes

--- src/test/java/com/gitlab/incognitojam/ext2/Ext2FileTest.java
+++ src/test/java/com/gitlab/incognitojam/ext2/Ext2FileTest.java
@@ -3,9 +3,11 @@ package com.gitlab.incognitojam.ext2;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static com.gitlab.incognitojam.ext2.Inode.FileModes.*;
 
 public class Ext2FileTest {
     private static final String volumeFilename = "ext2fs";
@@ -17,7 +19,24 @@ public class Ext2FileTest {
     }
 
     @Test
-    public void testRootFile() {
+    public void testRootFile() throws FileNotFoundException {
+        Ext2File root = new Ext2File(volume, "/");
+        assertEquals("/", root.getFileName());
+        assertEquals("/", root.getFilePath());
 
+        assertEquals(1024L, root.getSize());
+        assertEquals(6, root.getHardLinksCount());
+
+        assertEquals(1415188180L, root.getCreationTime());
+        assertEquals(1415188273L, root.getLastAccessTime());
+        assertEquals(1415188180L, root.getLastModifiedTime());
+        assertEquals(0L, root.getDeletedTime());
+
+        assertEquals(IFDIR | IXUSR | IWUSR | IRUSR | IXGRP | IRGRP | IXOTH | IROTH, root.getFileMode());
+        assertTrue(root.isDirectory());
+        assertFalse(root.isRegularFile());
+
+        assertEquals(0, root.getUnixUid());
+        assertEquals(0, root.getUnixGid());
     }
 }
