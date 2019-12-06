3ad9c512df611a0485d148bc17b70be8d3139597
Cameron Clough <cameronjclough@gmail.com>
2019-12-06 13:06:17 +0000

    test(ext2-file): add tests to read direct and indirect files

--- src/test/java/com/gitlab/incognitojam/ext2/Ext2FileTest.java
+++ src/test/java/com/gitlab/incognitojam/ext2/Ext2FileTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.junit.Assert.*;
 import static com.gitlab.incognitojam.ext2.Inode.FileModes.*;
+import static org.junit.Assert.*;
 
 public class Ext2FileTest {
     private static final String volumeFilename = "ext2fs";
@@ -64,5 +64,214 @@ public class Ext2FileTest {
          * in the file, if writing was supported.
          */
         root.seek(root.getSize() + 1024L);
+    }
+
+    @Test
+    public void testReadDirectStart() throws FileNotFoundException {
+        final byte[] expected = { 0x44, 0x69, 0x72, 0x65, 0x63, 0x74, 0x20, 0x73, 0x74, 0x61, 0x72, 0x74, 0x0a };
+
+        Ext2File file = new Ext2File(volume, "/files/dir-s");
+        byte[] data = file.read(file.getSize());
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadDirectEnd() throws FileNotFoundException {
+        final byte[] expected = new byte[0xb];
+        expected[0x0] = 0x44; // D
+        expected[0x1] = 0x69; // i
+        expected[0x2] = 0x72; // r
+        expected[0x3] = 0x65; // e
+        expected[0x4] = 0x63; // c
+        expected[0x5] = 0x74; // t
+        expected[0x6] = 0x20; //
+        expected[0x7] = 0x65; // e
+        expected[0x8] = 0x6e; // n
+        expected[0x9] = 0x64; // d
+        expected[0xa] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/dir-e");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadIndirectStart() throws FileNotFoundException {
+        final byte[] expected = new byte[0xf];
+        expected[0x0] = 0x49; // I
+        expected[0x1] = 0x6e; // n
+        expected[0x2] = 0x64; // d
+        expected[0x3] = 0x69; // i
+        expected[0x4] = 0x72; // r
+        expected[0x5] = 0x65; // e
+        expected[0x6] = 0x63; // c
+        expected[0x7] = 0x74; // t
+        expected[0x8] = 0x20; //
+        expected[0x9] = 0x73; // s
+        expected[0xa] = 0x74; // t
+        expected[0xb] = 0x61; // a
+        expected[0xc] = 0x72; // r
+        expected[0xd] = 0x74; // t
+        expected[0xe] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/ind-s");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadIndirectEnd() throws FileNotFoundException {
+        final byte[] expected = new byte[0xd];
+        expected[0x0] = 0x49; // I
+        expected[0x1] = 0x6e; // n
+        expected[0x2] = 0x64; // d
+        expected[0x3] = 0x69; // i
+        expected[0x4] = 0x72; // r
+        expected[0x5] = 0x65; // e
+        expected[0x6] = 0x63; // c
+        expected[0x7] = 0x74; // t
+        expected[0x8] = 0x20; //
+        expected[0x9] = 0x65; // e
+        expected[0xa] = 0x6e; // n
+        expected[0xb] = 0x64; // d
+        expected[0xc] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/ind-e");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadDoubleIndirectStart() throws FileNotFoundException {
+        final byte[] expected = new byte[0x16];
+        expected[0x00] = 0x44; // D
+        expected[0x01] = 0x6f; // o
+        expected[0x02] = 0x75; // u
+        expected[0x03] = 0x62; // b
+        expected[0x04] = 0x6c; // l
+        expected[0x05] = 0x65; // e
+        expected[0x06] = 0x20; //
+        expected[0x07] = 0x69; // i
+        expected[0x08] = 0x6e; // n
+        expected[0x09] = 0x64; // d
+        expected[0x0a] = 0x69; // i
+        expected[0x0b] = 0x72; // r
+        expected[0x0c] = 0x65; // e
+        expected[0x0d] = 0x63; // c
+        expected[0x0e] = 0x74; // t
+        expected[0x0f] = 0x20; //
+        expected[0x10] = 0x73; // s
+        expected[0x11] = 0x74; // t
+        expected[0x12] = 0x61; // a
+        expected[0x13] = 0x72; // r
+        expected[0x14] = 0x74; // t
+        expected[0x15] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/dbl-ind-s");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadDoubleIndirectEnd() throws FileNotFoundException {
+        final byte[] expected = new byte[0x14];
+        expected[0x00] = 0x44; // D
+        expected[0x01] = 0x6f; // o
+        expected[0x02] = 0x75; // u
+        expected[0x03] = 0x62; // b
+        expected[0x04] = 0x6c; // l
+        expected[0x05] = 0x65; // e
+        expected[0x06] = 0x20; //
+        expected[0x07] = 0x69; // i
+        expected[0x08] = 0x6e; // n
+        expected[0x09] = 0x64; // d
+        expected[0x0a] = 0x69; // i
+        expected[0x0b] = 0x72; // r
+        expected[0x0c] = 0x65; // e
+        expected[0x0d] = 0x63; // c
+        expected[0x0e] = 0x74; // t
+        expected[0x0f] = 0x20; //
+        expected[0x10] = 0x65; // e
+        expected[0x11] = 0x6e; // n
+        expected[0x12] = 0x64; // d
+        expected[0x13] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/dbl-ind-e");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadTripleIndirectStart() throws FileNotFoundException {
+        final byte[] expected = new byte[0x16];
+        expected[0x00] = 0x54; // T
+        expected[0x01] = 0x72; // r
+        expected[0x02] = 0x69; // i
+        expected[0x03] = 0x70; // p
+        expected[0x04] = 0x6c; // l
+        expected[0x05] = 0x65; // e
+        expected[0x06] = 0x20; //
+        expected[0x07] = 0x69; // i
+        expected[0x08] = 0x6e; // n
+        expected[0x09] = 0x64; // d
+        expected[0x0a] = 0x69; // i
+        expected[0x0b] = 0x72; // r
+        expected[0x0c] = 0x65; // e
+        expected[0x0d] = 0x63; // c
+        expected[0x0e] = 0x74; // t
+        expected[0x0f] = 0x20; //
+        expected[0x10] = 0x73; // s
+        expected[0x11] = 0x74; // t
+        expected[0x12] = 0x61; // a
+        expected[0x13] = 0x72; // r
+        expected[0x14] = 0x74; // t
+        expected[0x15] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/trpl-ind-s");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+        assertArrayEquals(expected, data);
+    }
+
+    @Test
+    public void testReadTripleIndirectEnd() throws FileNotFoundException {
+        final byte[] expected = new byte[0x14];
+        expected[0x00] = 0x54; // T
+        expected[0x01] = 0x72; // r
+        expected[0x02] = 0x69; // i
+        expected[0x03] = 0x70; // p
+        expected[0x04] = 0x6c; // l
+        expected[0x05] = 0x65; // e
+        expected[0x06] = 0x20; //
+        expected[0x07] = 0x69; // i
+        expected[0x08] = 0x6e; // n
+        expected[0x09] = 0x64; // d
+        expected[0x0a] = 0x69; // i
+        expected[0x0b] = 0x72; // r
+        expected[0x0c] = 0x65; // e
+        expected[0x0d] = 0x63; // c
+        expected[0x0e] = 0x74; // t
+        expected[0x0f] = 0x20; //
+        expected[0x10] = 0x65; // e
+        expected[0x11] = 0x6e; // n
+        expected[0x12] = 0x64; // d
+        expected[0x13] = 0x0a; // \n
+
+        Ext2File file = new Ext2File(volume, "/files/trpl-ind-e");
+        file.seek(file.getSize() - expected.length);
+        byte[] data = file.read(expected.length);
+
+        assertArrayEquals(expected, data);
     }
 }
